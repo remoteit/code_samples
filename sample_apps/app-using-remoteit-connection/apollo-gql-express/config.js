babc6a2e9c918b068598830799ae85950e738c9d
Brenda Strech <brenda@remote.it>
2022-06-13 13:55:35 -0700

    Add db config

--- sample_apps/app-using-remoteit-connection/apollo-gql-express/config.js
+++ sample_apps/app-using-remoteit-connection/apollo-gql-express/config.js
@@ -4,11 +4,13 @@ const MYSQL_URL = process.env.MYSQL_URL;
 const MYSQL_PORT = process.env.MYSQL_PORT;
 const MYSQL_USERNAME = process.env.MYSQL_USERNAME;
 const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
+const MYSQL_DB_NAME = process.env.MYSQL_DB_NAME;
 
 const config = {
   MYSQL_URL,
   MYSQL_PORT,
   MYSQL_USERNAME,
   MYSQL_PASSWORD,
+  MYSQL_DB_NAME,
 };
 module.exports = config;
