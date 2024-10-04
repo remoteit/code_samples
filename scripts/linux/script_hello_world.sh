24d2073b346ce4a07e225856985e94a3a79aefe1
Jamie Ruderman <jamieruderman@gmail.com>
2024-10-04 16:44:06 -0700

    Update script_hello_world.sh

--- scripts/linux/script_hello_world.sh
+++ scripts/linux/script_hello_world.sh
@@ -4,12 +4,10 @@
 set_attribute() {
     local name="$1"
     local value="$2"
-
-    # Send the data to the API, including jobDeviceId and attributeName in the URL
     curl -X POST "https://${GRAPHQL_API_PATH}/job/attribute/${JOB_DEVICE_ID}/${name}" \
          -H "Content-Type: text/plain" \
          --data "${value}"
 }
 
-# Create and set a hostname attribute
+# Get the hostname and set it as a device attribute
 set_attribute "hostname" "$(hostname)"
